addc7f443d31118c5b52d290cc9e0591963152fc
abhishek <mailabhipoear@gmail.com>
2020-07-22 23:45:33 +0530

    minor change (fixed variable name)

--- day22_zigzag_tre_traversal.py
+++ day22_zigzag_tre_traversal.py
@@ -2,25 +2,21 @@ class Solution:
     def zigzagLevelOrder(self, root):
         def height(root):
             if root:
-                return max(height(root.left),height(root.right))+1
+                return max(height(root.left), height(root.right))+1
             else:
                 return 0
 
-        def addToQueue(lvl,node):
-            if node and lvl<self.height:
+        def addToAns(lvl, node):
+            if node and lvl < self.height:
                 self.ans[lvl].append(node.val)
-                addToQueue(lvl+1,node.left)
-                addToQueue(lvl+1,node.right)
-                
-            
+                addToAns(lvl+1, node.left)
+                addToAns(lvl+1, node.right)
+
         self.height = height(root)
         self.ans = [list() for i in range(self.height)]
-        
-        from collections import deque
-        
 
-        addToQueue(0,root)
-        for idx,val in enumerate(self.ans):
-            if idx%2==1:
+        addToAns(0, root)
+        for idx, val in enumerate(self.ans):
+            if idx % 2 == 1:
                 self.ans[idx].reverse()
         return self.ans
