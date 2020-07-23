a22db84594b8cf85a97f20159d6907d7097579d1
abhishek <mailabhipoear@gmail.com>
2020-07-24 02:54:46 +0530

    hashset solution added

--- day23_count_unique_2.py
+++ day23_count_unique_2.py
@@ -4,3 +4,12 @@ class Solution:
         hmap = Counter(nums)
         a, b = hmap.most_common()[-2:]
         return [a[0], b[0]]
+
+        # better solution
+        s = set()
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+        return s
