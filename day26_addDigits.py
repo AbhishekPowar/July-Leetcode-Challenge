cee82728ef181c02761f610a631ed6ccd0416352
abhishek <mailabhipoear@gmail.com>
2020-07-26 20:29:07 +0530

    Math solution added

--- day26_addDigits.py
+++ day26_addDigits.py
@@ -4,9 +4,23 @@ class Solution:
         # summ = sum(map(int,list(str(num))))
         # return summ if summ<10 else self.addDigits(summ)
 
-        # efficient soln
+        # recursive solution
         summ = 0
         while num:
             summ += num % 10
             num = num//10
         return summ if summ < 10 else self.addDigits(summ)
+
+        # Iterative solution
+        while num >= 10:
+            summ = 0
+            while num:
+                summ += num % 10
+                num //= 10
+            num = summ
+        return num
+
+        if num:
+            return 9 if num % 9 == 0 else num % 9
+        else:
+            return 0
